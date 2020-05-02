5b30dadba9e35b0f63948be01cbf6e65096a1bdc
Stoian Minaiev <stoyan.minaev@gmail.com>
2020-05-02 11:34:44 +0300

    Added batch file

---
+++ db-bootstrap.bat
@@ -0,0 +1,18 @@
+@echo on
+
+rem Set environment
+set APPLICATION_DATAPATH="%APPDATA%\ChatBot"
+set APPLICATION_DATAVASE="chat-database.sqlite3"
+
+rem Show environment
+echo "Application Data Path is: %APPLICATION_DATAPATH%, Application Database is:  %APPLICATION_DATAVASE%"
+
+rem Create application data path directory if not exist
+if not exist "%APPLICATION_DATAPATH%" (
+    md "%APPLICATION_DATAPATH%"
+)
+
+rem Copy databasae into application data path directory
+copy "%APPLICATION_DATAVASE%" "%APPLICATION_DATAPATH%"
+
+pause
